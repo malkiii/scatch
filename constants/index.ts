6901789afd7247c18ce4f1f19e133217cf623dc3
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-17 10:49:33 +0100

    Create constants file.

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+export const imageSliderUrls = [
+  'https://images.pexels.com/photos/735987/pexels-photo-735987.jpeg',
+  'https://images.pexels.com/photos/6757020/pexels-photo-6757020.jpeg',
+  'https://images.pexels.com/photos/2506923/pexels-photo-2506923.jpeg',
+  'https://images.pexels.com/photos/1634187/pexels-photo-1634187.jpeg',
+  'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg',
+  'https://images.pexels.com/photos/441379/pexels-photo-441379.jpeg',
+  'https://images.pexels.com/photos/373934/pexels-photo-373934.jpeg',
+  'https://images.pexels.com/photos/2422915/pexels-photo-2422915.jpeg'
+];
