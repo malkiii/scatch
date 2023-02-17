927d0adb9d4d880ba97fe57018570deb3114d358
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-17 22:10:06 +0100

    Adding the fonts and slider animations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        dark: '#0a0a0a',
+        theme: 'rgb(34, 211, 238)'
+      },
+      fontFamily: {
+        monsterrat: ['Montserrat', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif']
+      },
+      borderRadius: {
+        circle: '50%'
+      },
+      animation: {
+        'scroll-left': 'scroll-left 30s linear infinite',
+        'scroll-right': 'scroll-right 30s linear infinite'
+      },
+      keyframes: {
+        'scroll-left': {
+          '0%': { translate: '0 0' },
+          '100%': { translate: '-50% 0' }
+        },
+        'scroll-right': {
+          '0%': { translate: '-50% 0' },
+          '100%': { translate: '0 0' }
+        }
+      }
+    }
   },
   plugins: []
 };
