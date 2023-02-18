534ad3be6199977c3f1c2c3b8859354b853578f2
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-18 21:09:25 +0100

    Adding images for search demo section.

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const imageSliderUrls = [
+export const sliderImages = [
   'https://images.pexels.com/photos/6867392/pexels-photo-6867392.jpeg',
   'https://images.pexels.com/photos/6757020/pexels-photo-6757020.jpeg',
   'https://images.pexels.com/photos/2506923/pexels-photo-2506923.jpeg',
@@ -8,3 +8,30 @@ export const imageSliderUrls = [
   'https://images.pexels.com/photos/373934/pexels-photo-373934.jpeg',
   'https://images.pexels.com/photos/2422915/pexels-photo-2422915.jpeg'
 ];
+
+export const searchDemoImages = {
+  nature: [
+    'https://images.pexels.com/photos/640781/pexels-photo-640781.jpeg',
+    'https://images.pexels.com/photos/13286992/pexels-photo-13286992.jpeg',
+    'https://images.pexels.com/photos/10568611/pexels-photo-10568611.jpeg',
+    'https://images.pexels.com/photos/8258044/pexels-photo-8258044.jpeg',
+    'https://images.pexels.com/photos/10903696/pexels-photo-10903696.jpeg',
+    'https://images.pexels.com/photos/12255770/pexels-photo-12255770.jpeg'
+  ],
+  wallpapers: [
+    'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg',
+    'https://images.pexels.com/photos/5117558/pexels-photo-5117558.jpeg',
+    'https://images.pexels.com/photos/6012907/pexels-photo-6012907.jpeg',
+    'https://images.pexels.com/photos/983200/pexels-photo-983200.jpeg',
+    'https://images.pexels.com/photos/130621/pexels-photo-130621.jpeg',
+    'https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg'
+  ],
+  vintage: [
+    'https://images.pexels.com/photos/859895/pexels-photo-859895.jpeg',
+    'https://images.pexels.com/photos/3633990/pexels-photo-3633990.jpeg',
+    'https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg',
+    'https://images.pexels.com/photos/14552610/pexels-photo-14552610.jpeg',
+    'https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg',
+    'https://images.pexels.com/photos/92866/pexels-photo-92866.jpeg'
+  ]
+};
