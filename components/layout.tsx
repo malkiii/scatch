f1d3fac823ab436c5ae01a58af66831ad0d48fdb
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-18 16:39:55 +0100

    Including the favicons.

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,21 @@ export default function Layout({ children }: any) {
     <>
       <Head>
         <title>Scatch</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="favicon-dark.ico"
+          media="(prefers-color-scheme: no-preference)"
+        />
+        <link
+          rel="icon"
+          href="favicon-light.ico"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href="favicon-dark.ico"
+          media="(prefers-color-scheme: dark)"
+        />
       </Head>
       <Navbar />
       {children}
