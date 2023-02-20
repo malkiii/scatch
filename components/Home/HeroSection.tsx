3cea911bcedc6fb6cba59834f2798b7a7a005f63
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-20 18:13:24 +0100

    Updating the styles.

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -4,16 +4,15 @@ export default function HeroSection(): JSX.Element {
   return (
     <div className="py-20">
       <div className="text-center mx-auto max-w-5xl">
-        <h1 className="text-7xl font-extrabold my-8">
+        <h1 className="my-8">
           welcome to{' '}
           <span className="relative inline-block text-transparent bg-clip-text theme-gradient">
             Scatch
           </span>
         </h1>
-        <p className="text-2xl font-rubik opacity-75 leading-relaxed mb-10">
-          Scatch is an online image gallery were you can find your favored
-          images and edit it, save it or download it for free with high
-          resolusion.
+        <p className="text-2xl leading-relaxed mb-10 opacity-75">
+          Scatch is an online image gallery where you can find your favored
+          images and edit, save, or download them for free with high resolution.
         </p>
       </div>
       <InfinitImageSlider />
