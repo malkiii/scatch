84692dd50db15596eeba909b66413cb6e3510c68
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-23 10:04:01 +0100

    Change the spacing.

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -2,20 +2,23 @@ import InfinitImageSlider from './InfinitImageSlider';
 
 export default function HeroSection(): JSX.Element {
   return (
-    <div className="py-20">
-      <div className="text-center mx-auto max-w-5xl">
-        <h1 className="my-8">
-          welcome to{' '}
-          <span className="relative inline-block text-transparent bg-clip-text theme-gradient">
-            Scatch
-          </span>
-        </h1>
-        <p className="text-2xl leading-relaxed mb-10 opacity-75">
-          Scatch is an online image gallery where you can find your favored
-          images and edit, save, or download them for free with high resolution.
-        </p>
+    <div className="py-20 px-8 overflow-hidden">
+      <div className="flex justify-center items-center gap-8 mx-auto max-w-7xl">
+        <div className="w-1/2">
+          <h1 className="my-4">
+            welcome to{' '}
+            <span className="relative inline-block text-transparent bg-clip-text theme-gradient">
+              Scatch
+            </span>
+          </h1>
+          <p className="text-2xl leading-relaxed mb-8 opacity-75">
+            Scatch is an online image gallery where you can find your favored
+            images and edit, save, or download them for free with high
+            resolution.
+          </p>
+        </div>
+        <InfinitImageSlider />
       </div>
-      <InfinitImageSlider />
     </div>
   );
 }
