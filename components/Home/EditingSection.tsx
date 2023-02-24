1d5bc264c8b50f63838f4db5ae9d231c9c6ebca6
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-24 22:41:05 +0100

    Finishing the section.

--- components/Home/EditingSection.tsx
+++ components/Home/EditingSection.tsx
@@ -1,66 +1,52 @@
 import Image from 'next/image';
-import { motion, useAnimationControls } from 'framer-motion';
-import { albumsSectionImages } from '../../constants';
-import { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import ThemeButton from '../ThemeButton';
 
-const albumsNumber = albumsSectionImages.length;
+const filters = [
+  'after:opacity-0 after:mix-blend-difference',
+  'after:opacity-60 after:mix-blend-overlay after:bg-amber-900',
+  'after:opacity-0 after:mix-blend-overlay grayscale contrast-125',
+  'after:opacity-25 after:mix-blend-difference after:bg-theme contrast-150'
+];
 
 export default function EditingSection() {
-  const controls = useAnimationControls();
+  const [currentFilterIndex, setCurrentFilterIndex] = useState<number>(0);
 
-  const sequence = async () => {
-    const startX = albumsNumber * 10;
-    const startY = 100 - albumsNumber * 10;
-    await controls.start(i => ({
-      x: `-${startX}%`,
-      y: `-${startY}%`,
-      transition: { delay: i * 0.1, ease: 'anticipate', duration: 1.5 }
-    }));
-    await controls.start(i => ({
-      x: `-${startX + i * 10}%`,
-      y: `-${startY - i * 10}%`,
-      transition: { ease: 'anticipate', duration: 0.6 }
-    }));
-  };
   useEffect(() => {
-    sequence();
+    const interval = setInterval(() => {
+      setCurrentFilterIndex(
+        currentFilterIndex == filters.length - 1 ? 0 : currentFilterIndex + 1
+      );
+    }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
   });
 
   return (
-    <div className="flex items-center justify-center py-10 flex-wrap gap-x-10 overflow-hidden">
-      <div className="max-w-[500px]">
-        <h2>Create your own albums.</h2>
-        <p className="text-xl">
-          After creating your account, you can save images in albums which you
-          can check later.
-        </p>
+    <div className="flex items-center justify-center py-20 gap-x-10">
+      <div className="relative aspect-[609/761] w-[400px] overflow-hidden">
+        <motion.div
+          className={
+            'absolute w-full h-full transition-all duration-1000 after:transition-inherit after:absolute after:w-full after:h-full ' +
+            filters[currentFilterIndex]
+          }
+        >
+          <Image
+            src="https://images.pexels.com/photos/7537866/pexels-photo-7537866.jpeg"
+            alt="editing-image"
+            fill
+          />
+        </motion.div>
       </div>
-      <div className="relative h-[400px] w-[500px]">
-        {albumsSectionImages.map((imageUrl, index) => {
-          return (
-            <motion.div
-              key={index}
-              custom={index}
-              animate={controls}
-              className="album-example"
-              style={{
-                zIndex: index,
-                x: '100vw',
-                y: '-60%'
-              }}
-            >
-              <Image
-                src={`${imageUrl}?auto=compress&cs=tinysrgb&w=480`}
-                alt={'album-' + index}
-                className="w-full rounded-inherit"
-                fill
-              />
-              <span className="absolute bottom-3 left-4 opacity-70 font-bold">
-                album
-              </span>
-            </motion.div>
-          );
-        })}
+      <div className="max-w-[550px]">
+        <h2>Upload and Edit the images.</h2>
+        <p className="text-xl mb-6">
+          Upload images to the albums you create, and edit them with many
+          features and filters.
+        </p>
+        <ThemeButton text="Edit some pictures" />
       </div>
     </div>
   );
