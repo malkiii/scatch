b0f603a19acbd8431f2b606423bb1763ce1a0f33
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-25 23:31:38 +0100

    Updating the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,7 @@ export default function Navbar(): JSX.Element {
     <header className="h-20 w-full px-8">
       <nav className="flex items-center justify-between max-w-7xl h-full mx-auto">
         <Link href="/">
-          <Image
-            src="/logotype.svg"
-            className="relative"
-            alt="Logo"
-            width={144}
-            height={36}
-          />
+          <Image src="/logotype.svg" alt="Logo" width={144} height={36} />
         </Link>
         <div>
           <Link href="/" className="theme-link">
