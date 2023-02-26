2091db30480f26d12e118979274a9db6d3b6e808
malkiAbdoo <abdo.malkiep@gmail.com>
2023-02-26 22:23:41 +0100

    Adding the a start animation.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,30 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 import ThemeButton from './ThemeButton';
+import { easeInOutExpo } from '../constants';
+
+const navVariants = {
+  hidden: { y: 200, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.9,
+      ease: easeInOutExpo
+    }
+  }
+};
 
 export default function Navbar(): JSX.Element {
   return (
-    <header className="h-20 w-full px-8">
-      <nav className="flex items-center justify-between max-w-7xl h-full mx-auto">
+    <header className="relative h-20 w-full px-8">
+      <motion.nav
+        variants={navVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex items-center justify-between max-w-7xl h-full mx-auto overflow-y-hidden"
+      >
         <Link href="/">
           <Image src="/logotype.svg" alt="Logo" width={144} height={36} />
         </Link>
@@ -18,7 +37,7 @@ export default function Navbar(): JSX.Element {
           </Link>
           <ThemeButton text="Login" />
         </div>
-      </nav>
+      </motion.nav>
     </header>
   );
 }
