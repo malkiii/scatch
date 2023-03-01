55bcfc206f1ee72990959a7bc4b99e0345fab60b
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-01 22:01:20 +0100

    Set the dark theme.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head />
-      <body className="bg-dark text-white font-monsterrat overflow-x-hidden">
+      <body className="bg-white dark:bg-dark text-dark dark:text-white font-monsterrat overflow-x-hidden transition-colors">
         <Main />
         <NextScript />
       </body>
