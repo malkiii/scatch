77f2bef60307c2500d1f35834c26d21f0466b3c7
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-02 20:28:18 +0100

    Creating dark theme toggle button🔲🔳

--- components/DarkThemeButton.tsx
+++ components/DarkThemeButton.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import { FC } from 'react';
 
 type ButtonProps = {
   containerClassName: string;
   buttonClassName: string;
 };
 
-export default function DarkThemeButton(props: ButtonProps): JSX.Element {
-  const [darkTheme, setDarkTheme] = useState<boolean>(false);
+export const DarkThemeButton: FC<ButtonProps> = props => {
   function toggleDarkTheme() {
-    setDarkTheme(!darkTheme);
+    document.documentElement.classList.toggle('dark');
   }
 
   return (
@@ -22,10 +21,11 @@ export default function DarkThemeButton(props: ButtonProps): JSX.Element {
       dark
       <div
         className={
-          'relative rounded-2xl after:absolute after:top-1/2 after:translate-y-[-48%] after:left-px after:h-[90%] after:rounded-circle after:aspect-square group-hover:border-theme group-hover:after:bg-theme transition-inherit after:transition-inherit border-2 border-white after:bg-white ' +
+          'relative rounded-2xl after:absolute after:top-1/2 after:translate-y-[-48%] after:h-[90%] after:rounded-circle after:aspect-square group-hover:border-theme group-hover:after:bg-theme transition-inherit after:transition-inherit after:duration-500 border-2 dark:border-white border-dark dark:after:bg-white after:bg-dark dark:after:left-px after:left-[58%] ' +
           props.buttonClassName
         }
       ></div>
     </button>
   );
-}
+};
+export default DarkThemeButton;
