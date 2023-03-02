5438319fc6ff423abdd788818f80a31ad53adf48
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-02 20:21:39 +0100

    Social Media accounts.

---
+++ constants/socials.tsx
@@ -0,0 +1,24 @@
+import { FaFacebook, FaTwitter, FaInstagram, FaGithub } from 'react-icons/fa';
+
+export const socials = [
+  {
+    name: 'Facebook',
+    icon: <FaFacebook />,
+    url: 'https://web.facebook.com/malkii.abdoo'
+  },
+  {
+    name: 'Twitter',
+    icon: <FaTwitter />,
+    url: 'https://twitter.com/malkiAbdoo'
+  },
+  {
+    name: 'Instagram',
+    icon: <FaInstagram />,
+    url: 'https://www.instagram.com/malki.abdoo'
+  },
+  {
+    name: 'Github',
+    icon: <FaGithub />,
+    url: 'https://github.com/malkiAbdoo'
+  }
+];
