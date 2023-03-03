6c022803bbd149bbf2c69e4bbcfb4ea2f9fde2bc
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-03 23:05:02 +0100

    get the theme value in LS.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import { AppProps } from 'next/app';
 import Layout from '../components/layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  if (typeof window !== 'undefined') {
+    const theme = localStorage.getItem('theme') || 'dark';
+    if (theme != 'dark') document.documentElement.classList.toggle('dark');
+    if (!theme) {
+      localStorage.setItem('theme', 'dark');
+    }
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
