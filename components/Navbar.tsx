9eb58172777249addc388591086e038d9194f9a9
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-07 22:17:08 +0100

    Creating the login button.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
-import { easeInOutExpo } from '../constants';
 import { useState } from 'react';
 import NavbarMenu from './NavbarMenu';
+import { motion } from 'framer-motion';
+import { easeInOutExpo } from '../constants';
 import DarkThemeButton from './DarkThemeButton';
+import { withRouter, NextRouter } from 'next/router';
+
+type NavbarProps = {
+  router: NextRouter;
+};
 
 const navVariants = {
   hidden: { y: 200, opacity: 0 },
@@ -18,34 +23,45 @@ const navVariants = {
   }
 };
 
-export default function Navbar(): JSX.Element {
+function Navbar(props: NavbarProps) {
   const [open, setOpen] = useState<boolean>(false);
+
+  const animationProps =
+    props.router.pathname == '/'
+      ? { variants: navVariants, initial: 'hidden', animate: 'visible' }
+      : null;
+
   return (
     <>
       <header className="relative h-20 w-full px-8">
+        {undefined}
         <motion.nav
-          variants={navVariants}
-          initial="hidden"
-          animate="visible"
+          {...animationProps}
           className="flex items-center justify-between max-w-7xl h-full mx-auto overflow-y-hidden"
         >
           <Link href="/">
-            <Image src="/logotype.svg" alt="Logo" width={144} height={36} />
+            <Image
+              src="/logotype.svg"
+              alt="Logo"
+              width={144}
+              height={39}
+              className="logo"
+            />
           </Link>
           <div className="hidden md:block">
-            <Link href="/" className="theme-link">
-              Explore
+            <Link href="/search" className="theme-link">
+              Search
             </Link>
-            <Link href="/" className="theme-link">
+            <Link href="/albums" className="theme-link">
               My albums
             </Link>
-            <Link href="/" className="theme-link">
-              About
-            </Link>
             <DarkThemeButton
-              containerClassName="inline-flex items-center gap-3 pl-10 border-l-2 border-white/40"
+              containerClassName="inline-flex items-center gap-3 mr-10"
               buttonClassName="w-10 h-5"
             />
+            <Link href="/" className="theme-btn">
+              Login
+            </Link>
           </div>
           <button
             className="nav-menu-btn"
@@ -57,3 +73,4 @@ export default function Navbar(): JSX.Element {
     </>
   );
 }
+export default withRouter(Navbar);
