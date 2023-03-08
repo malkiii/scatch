1989c2e2a77c31f380f0265ccef2a176f0a69568
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-08 22:52:30 +0100

    Removing the theme legacy code.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,9 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import Layout from '../components/layout';
-import { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // set the theme with the 'dark' class
-  function applyTheme() {
-    const theme = localStorage.getItem('theme') || 'dark';
-    document.documentElement.setAttribute('data-theme', theme);
-  }
-
-  useEffect(() => {
-    applyTheme();
-  });
-
   return (
     <ThemeProvider attribute="class">
       <Layout>
