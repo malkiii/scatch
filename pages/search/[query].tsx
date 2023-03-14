01fc226f8f95f2a106a64141fd2be11b33bfdb00
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-14 22:48:37 +0100

    Using the orientation in the server side props.

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,130 +1,68 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { NextPage, GetServerSideProps } from 'next';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import { Dispatch, FC, SetStateAction } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import FilterMenu from '../../components/Search/FilterMenu';
 import ImageLayout from '../../components/Search/ImageLayout';
 import SearchSection from '../../components/Search/SearchSection';
-import { useFetch, ResponseImage, fetchImages } from '../../hooks/useFetch';
-import { BsAspectRatio } from 'react-icons/bs';
 import ScrollToTopButton from '../../components/ScrollToTopButton';
+import { useFetch, ResponseImage, fetchImages } from '../../hooks/useFetch';
 
 type Props = {
-  query: string;
+  searchQuery: string;
   images: ResponseImage[];
-  inLastPage: boolean;
+  hasMore: boolean;
 };
 
-type FilterMenuProps = {
-  setOption: Dispatch<SetStateAction<string>>;
+type RouteQuery = {
+  query: string;
+  o: string;
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const query = params!.query as string;
-  const searchParams = new URLSearchParams({ e: 'search', q: query });
-  const { newImages, inLastPage } = await fetchImages(searchParams);
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { query: searchQuery, o: orientation } = query as RouteQuery;
+  const searchParams = new URLSearchParams({ e: 'search', q: searchQuery });
+  if (orientation) searchParams.append('o', orientation);
+  const id = searchQuery + (orientation || '');
+
+  const { newImages, hasMore } = await fetchImages(searchParams);
   return {
-    props: {
-      query,
-      images: newImages,
-      inLastPage,
-      key: query
-    }
+    props: { searchQuery, images: newImages, hasMore, key: id }
   };
 };
 
-const transition = { type: 'tween', duration: 0.1 };
-const menuVarinats = {
-  hidden: { opacity: 0, transition },
-  visible: { opacity: 1, transition }
-};
-
-const FilterMenu: FC<FilterMenuProps> = ({ setOption }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  function toggleTheMenu() {
-    setIsOpen(!isOpen);
-  }
-
-  const optionList = ['all', 'portrait', 'landscape'];
-  function changeOptionTo(option: string) {
-    setOption(option);
-    toggleTheMenu();
-  }
-  return (
-    <div className="relative">
-      <button
-        className="flex items-center gap-1 border-2 p-2 rounded-md bg-white dark:bg-neutral-900 px-4 dark:border-white/40 dark:hover:border-white transition-colors group border-dark/40 hover:border-dark"
-        onClick={toggleTheMenu}
-      >
-        Filter
-        <MdKeyboardArrowDown
-          size={22}
-          className={
-            'transition-all dark:text-white/40 dark:group-hover:text-white text-dark/40 group-hover:text-dark ' +
-            (isOpen ? 'rotate-180' : '')
-          }
-        />
-      </button>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            className="absolute top-full translate-y-2 right-0 z-10 shadow-3xl"
-            variants={menuVarinats}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-          >
-            <ul>
-              {optionList.map((option, index) => (
-                <li
-                  key={index}
-                  className="filter-option group"
-                  onClick={() => changeOptionTo(option)}
-                >
-                  {option != 'all' && (
-                    <BsAspectRatio
-                      className={option == 'portrait' ? 'rotate-90' : ''}
-                    />
-                  )}
-
-                  {option}
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
-
-const SearchResults: NextPage<Props> = ({ query, images, inLastPage }) => {
+const SearchResults: NextPage<Props> = ({ searchQuery, images, hasMore }) => {
+  const router = useRouter();
   const hasResults = images.length > 0;
-  const [option, setOption] = useState('all');
-  const imageArray = useFetch('search', inLastPage, query, images, option);
+  const orientation = (router.query!.o as string) || 'all';
+  const imageArray = useFetch(
+    'search',
+    hasMore,
+    searchQuery,
+    images,
+    orientation
+  );
   return (
     <>
       <Head>
-        <title>{query} images | Search and save in your albums</title>
+        <title>{searchQuery} images | Search and save in your albums</title>
       </Head>
       <div className="px-8">
-        <SearchSection value={query} />
+        <SearchSection value={searchQuery} />
         <div className="max-w-[1250px] mx-auto">
           {hasResults ? (
             <>
               <div className="flex items-center justify-between w-full mb-5">
                 <h3 className="font-bold text-xl sm:text-2xl lg:text-4xl mb-5 first-letter:capitalize">
-                  {query} images.
+                  {searchQuery} images.
                 </h3>
-                <FilterMenu setOption={setOption} />
+                <FilterMenu query={searchQuery} focusOn={orientation} />
               </div>
               <ImageLayout images={imageArray} />
             </>
           ) : (
             <>
               <h3 className="text-2xl sm:text-3xl lg:text-5xl mb-5 first-letter:capitalize">
-                No results for “{query}”.
+                No results for “{searchQuery}”.
               </h3>
               <p className="text-xl sm:text-2xl lg:text-4xl">
                 Try another search.
