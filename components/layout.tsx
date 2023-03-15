8a3551fdb753f1c253ac10fe59debf4abd15c111
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-15 21:27:29 +0100

    Handle the favicon theme 🔲

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,26 +1,18 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
 import { FC, ReactNode } from 'react';
+import { useTheme } from 'next-themes';
 
 type layoutProps = {
   children: ReactNode;
 };
 
 const Layout: FC<layoutProps> = ({ children }) => {
+  const { systemTheme } = useTheme();
   return (
     <>
       <Head>
-        <title>Scatch</title>
-        <link
-          rel="icon"
-          href="/favicon-light.ico"
-          media="(prefers-color-scheme: light)"
-        />
-        <link
-          rel="icon"
-          href="/favicon-dark.ico"
-          media="(prefers-color-scheme: dark)"
-        />
+        <link rel="icon" href={`/favicon-${systemTheme || 'light'}.ico`} />
       </Head>
       <Navbar />
       {children}
