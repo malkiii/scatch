1d5ac8dcf09e46e495aace1fa161135673077743
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-15 21:31:24 +0100

    Set the page title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { NextPage } from 'next';
 import Footer from '../components/Footer';
 import HeroSection from '../components/Home/HeroSection';
@@ -9,6 +10,9 @@ import ScrollToTopButton from '../components/ScrollToTopButton';
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Scatch</title>
+      </Head>
       <div className="px-8">
         <HeroSection />
         <ExploreSection />
