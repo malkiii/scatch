766a11ccc216129acc4c95e006b84d5ef6fabc73
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-23 12:15:16 +0100

    Moving the image layer to another file.

--- components/Search/ImageLayout.tsx
+++ components/Search/ImageLayout.tsx
@@ -1,49 +1,19 @@
+import Link from 'next/link';
 import Image from 'next/image';
-import { FC, useState, useRef, useEffect } from 'react';
+import ImageLayer from './ImageLayer';
 import { ResponseImage } from '../../hooks/useFetch';
-import { CgMathPlus, CgSoftwareDownload } from 'react-icons/cg';
+import { FC, useState, useRef, useEffect } from 'react';
 
 type ImageLayoutProps = {
+  pathname: string;
   images: ResponseImage[];
-};
-
-type ImageCoverProps = {
-  image: ResponseImage;
-};
-
-const ImageCover: FC<ImageCoverProps> = ({ image }) => {
-  const logoSize = 25;
-  const downloadURL = `${image.src}?cs=srgb&dl=scatch-${image.id}.jpg&fm=jpg`;
-  return (
-    <>
-      <a
-        href={downloadURL}
-        className="sm:hidden absolute bottom-5 right-5 text-white hover:text-theme transition-colors p-2"
-      >
-        <CgSoftwareDownload size={40} />
-      </a>
-      <div className="hidden sm:block image-layout-cover">
-        <a href="" className="image-layout-btn absolute top-5 right-5">
-          <CgMathPlus size={logoSize} />
-        </a>
-        <div className="absolute w-full bottom-0 p-5 flex items-center justify-between">
-          <strong className="font-normal text-white">
-            By {image.photographer}
-          </strong>
-          <a href={downloadURL} className="image-layout-btn">
-            <CgSoftwareDownload size={logoSize} />
-          </a>
-        </div>
-      </div>
-    </>
-  );
 };
 
 const PreloadLayout: FC = () => {
   return (
     <>
       {new Array(3).fill(null).map((_, col) => (
-        <div key={col} className="grid grid-cols-1 flex-grow gap-y-4">
+        <div key={col} className="grid grid-cols-1 gap-y-4">
           {new Array(3).fill(null).map((_, row) => (
             <div key={col + row * 3} className="h-96 bg-neutral-500/5"></div>
           ))}
@@ -53,7 +23,7 @@ const PreloadLayout: FC = () => {
   );
 };
 
-const ImageLayout: FC<ImageLayoutProps> = ({ images }) => {
+const ImageLayout: FC<ImageLayoutProps> = ({ pathname, images }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [columnsNumber, setColumnsNumber] = useState<number>(0);
 
@@ -83,25 +53,30 @@ const ImageLayout: FC<ImageLayoutProps> = ({ images }) => {
     >
       {columnsNumber ? (
         new Array(columnsNumber).fill(null).map((_, col) => (
-          <div key={col} className="grid grid-cols-1 flex-grow gap-y-4">
+          <div key={col} className="grid grid-cols-1 gap-y-4">
             {new Array(getRowsNumber()).fill(null).map((_, row) => {
               const imageIndex = col + row * columnsNumber;
               const currentImage = images[imageIndex];
               if (!currentImage) return <></>;
+
+              const imageURL = `${currentImage.src}?auto=compress&cs=tinysrgb&w=940`;
               return (
-                <div
+                <Link
                   key={currentImage.id}
+                  href={pathname}
+                  as={`/search/scatch-${currentImage.id}`}
                   className="relative"
                   style={{ backgroundColor: currentImage.avgColor }}
+                  shallow
                 >
                   <Image
-                    src={currentImage.src + '?auto=compress&cs=tinysrgb&w=940'}
+                    src={imageURL}
                     width={currentImage.width}
                     height={currentImage.height}
                     alt="scatch image"
                   />
-                  <ImageCover image={currentImage} />
-                </div>
+                  <ImageLayer image={currentImage} />
+                </Link>
               );
             })}
           </div>
