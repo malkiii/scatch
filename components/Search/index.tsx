08e361067034f1182968907073b821a6101216c0
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-27 23:17:02 +0100

    Using the multi-export for the page components.

---
+++ components/Search/index.tsx
@@ -0,0 +1,4 @@
+export { default as SearchInput } from './SearchInput';
+export { default as SearchKeywords } from './SearchKeywords';
+export { default as FilterMenu } from './FilterMenu';
+export { default as ImageGridLayout } from './ImageGridLayout';
