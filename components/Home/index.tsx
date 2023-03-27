0479ace4881e3df031540e6f5998829ec697ca94
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-27 23:16:50 +0100

    Using the multi-export for the page components.

---
+++ components/Home/index.tsx
@@ -0,0 +1,4 @@
+export { default as ExploreSection } from './ExploreSection';
+export { default as AlbumsSection } from './AlbumsSection';
+export { default as EditingSection } from './EditingSection';
+export { default as HeroSection } from './HeroSection';
