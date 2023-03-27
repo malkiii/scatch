b20dfcdcd38bd01fd56b240b9cf3c7b266f48a03
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-27 23:10:34 +0100

    Create the search page.

---
+++ pages/search.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import { NextPage, GetServerSideProps } from 'next';
+import ImageGridLayout from '../components/Search/ImageGridLayout';
+import SearchInput from '../components/Search/SearchInput';
+import ScrollToTopButton from '../components/ScrollToTopButton';
+import SearchKeywords from '../components/Search/SearchKeywords';
+import { useFetch, ResponseImage, fetchImages } from '../hooks/useFetch';
+
+type Props = {
+  images: ResponseImage[];
+  hasMore: boolean;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const searchParams = new URLSearchParams({ e: 'curated' });
+  const { newImages, hasMore } = await fetchImages(searchParams);
+  return {
+    props: { images: newImages, hasMore }
+  };
+};
+
+const Search: NextPage<Props> = ({ images, hasMore }) => {
+  const params = {
+    endpoint: 'curated',
+    initialImages: images,
+    hasMore
+  };
+
+  const imageArray = useFetch(params);
+  return (
+    <div className="px-8">
+      <Head>
+        <title>Search for images</title>
+      </Head>
+      <SearchInput />
+      <div className="max-w-screen-xl mx-auto">
+        <SearchKeywords />
+        <ImageGridLayout fullPath="/search" images={imageArray} />
+      </div>
+      <ScrollToTopButton />
+    </div>
+  );
+};
+export default Search;
