911706beef0d3c1e3750a2bf0473d430e0dee948
malkiAbdoo <abdo.malkiep@gmail.com>
2023-03-31 17:35:02 +0100

    Update the button styles

--- components/DarkThemeButton.tsx
+++ components/DarkThemeButton.tsx
@@ -1,13 +1,14 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { useTheme } from 'next-themes';
 
 type ButtonProps = {
-  containerClassName: string;
-  buttonClassName: string;
+  className: string;
+  children?: ReactNode;
 };
 
-const DarkThemeButton: FC<ButtonProps> = props => {
+const DarkThemeButton: FC<ButtonProps> = ({ className, children }) => {
   const { setTheme } = useTheme();
+
   function toggleTheme() {
     const preferDarkTheme = document.documentElement.classList.contains('dark');
     setTheme(preferDarkTheme ? 'light' : 'dark');
@@ -16,20 +17,12 @@ const DarkThemeButton: FC<ButtonProps> = props => {
   return (
     <button
       className={
-        'group hover:text-theme transition-colors duration-200 ' +
-        props.containerClassName
+        'group hover:text-theme transition-colors duration-200 ' + className
       }
       onClick={toggleTheme}
     >
-      dark
-      <div
-        className={
-          'relative flex items-center rounded-2xl group-hover:border-theme transition-inherit border-2 dark:border-white border-dark  p-[2px] pt-[3px] ' +
-          props.buttonClassName
-        }
-      >
-        <div className="h-full aspect-square rounded-circle bg-dark dark:bg-white group-hover:bg-theme transition-inherit ml-auto dark:ml-0"></div>
-      </div>
+      {children}
+      <div className="relative before:inline-block before:font-theme before:not-italic before:antialiased before:aspect-square before:font-normal before:content-dark dark:before:content-light before:mt-1"></div>
     </button>
   );
 };
