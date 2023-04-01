e853398727d3a2252462151298c2a2316499e0de
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-01 22:25:27 +0100

    - Moving the easing function to easing.ts.

--- constants/index.ts
+++ constants/index.ts
@@ -24,17 +24,3 @@ export const searchDemoImages = {
     'https://images.pexels.com/photos/92866/pexels-photo-92866.jpeg'
   ]
 };
-
-// animation timing functions
-export function easeInOutExpo(x: number): number {
-  return x == 0
-    ? 0
-    : x == 1
-    ? 1
-    : x < 0.5
-    ? Math.pow(2, 20 * x - 10) / 2
-    : (2 - Math.pow(2, -20 * x + 10)) / 2;
-}
-export function easeOutExpo(x: number): number {
-  return x == 1 ? 1 : 1 - Math.pow(2, -10 * x);
-}
