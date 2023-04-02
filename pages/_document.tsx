75bee002d47b2c1d4bd60a7796119c3980690a7c
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-02 17:49:20 +0100

    - Using the color-scheme-icon CDN for the theme icons.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/malkiAbdoo/color-scheme-icon-cdn/cs-icon.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
