1fa3ecc2dd165bc7da1bc50faf409f60376813a0
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-06 14:00:06 +0100

    - handle menu toggling.

---
+++ hooks/useToggleMenu.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useToggleMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  function toggle() {
+    setIsOpen(!isOpen);
+  }
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const menuContainer = menuRef.current!;
+    const clickOnTheMenu = menuContainer.contains(event.target as Node);
+    if (!clickOnTheMenu) setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return { isOpen, menuRef, toggle };
+};
