0823b1a8f31e1c64bfa79f61015df967b3c138f8
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-06 13:56:38 +0100

    - Keep track of the image grid layout columns.

---
+++ hooks/useGridColumnsNumber.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useGridColumnsNumber = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [columnsNumber, setColumnsNumber] = useState<number>(0);
+
+  function updateColumnsNumber() {
+    const container = containerRef.current;
+    if (container) {
+      const containerStyle = window.getComputedStyle(container);
+      setColumnsNumber(containerStyle.gridTemplateColumns.split(' ').length);
+    }
+  }
+
+  useEffect(() => {
+    updateColumnsNumber();
+    window.addEventListener('resize', updateColumnsNumber);
+    return () => {
+      window.removeEventListener('resize', updateColumnsNumber);
+    };
+  }, []);
+
+  return { containerRef, columnsNumber };
+};
