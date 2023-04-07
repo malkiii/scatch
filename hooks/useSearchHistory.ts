f20c1a89a276333ed03f2a87ada30bba61793562
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-07 12:26:44 +0100

    - display the search history 🕑 on Input.

---
+++ hooks/useSearchHistory.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+type SearchHistory = string[];
+
+export const useSearchHistory = (prefix = '') => {
+  const [searchValues, setSearchValues] = useState<SearchHistory>([]);
+
+  function getSearchHistory(): SearchHistory {
+    const searchHistory = localStorage.getItem('search_history');
+    const values: SearchHistory = JSON.parse(searchHistory || '[]');
+    return values.filter(value => value.startsWith(prefix));
+  }
+
+  useEffect(() => {
+    setSearchValues(getSearchHistory());
+  }, [prefix]);
+
+  function remove(value: string) {
+    const index = searchValues.indexOf(value);
+    const newArray = searchValues;
+    newArray.splice(index, 1);
+    setSearchValues(newArray);
+  }
+
+  return searchValues;
+};
