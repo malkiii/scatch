f6609d106fba1fd4534882b29a9a71ea568ff4b9
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-11 18:20:32 +0100

    - Create the fetch photo function for the /image/id route.

--- utils/fetchImages.ts
+++ utils/fetchImages.ts
@@ -5,33 +5,54 @@ type ResponseData = {
   hasMore: boolean;
 };
 
+const HOSTNAME = process.env.HOSTNAME as string;
+const API_TOKEN = process.env.API_TOKEN as string;
+const headers = { token: API_TOKEN };
+
+function extractImageObject(data: Record<string, any>): ResponseImage {
+  return {
+    id: data.id,
+    width: data.width,
+    height: data.height,
+    photographer: data.photographer,
+    avgColor: data.avg_color,
+    src: data.src.original
+  };
+}
+
 export const fetchImages = async (
   params: Record<string, string>,
   signal?: AbortSignal
 ): Promise<ResponseData> => {
   try {
-    const HOSTNAME = process.env.HOSTNAME;
-    const API_TOKEN = process.env.API_TOKEN as string;
-
     const endpointURL = new URL('/api/images', HOSTNAME);
     endpointURL.search = new URLSearchParams(params).toString();
-
-    const headers = { token: API_TOKEN };
 
     const response = await fetch(endpointURL, { headers, signal });
     const data = await response.json();
 
-    const images: ResponseImage[] = data.photos.map((image: any) => ({
-      id: image.id,
-      width: image.width,
-      height: image.height,
-      photographer: image.photographer,
-      avgColor: image.avg_color,
-      src: image.src.original
-    }));
+    const images: ResponseImage[] = data.photos.map((image: any) =>
+      extractImageObject(image)
+    );
 
     return { images, hasMore: 'next_page' in data };
   } catch (error) {
     return { images: [], hasMore: false };
+  }
+};
+
+export const fetchPhoto = async (id: string) => {
+  try {
+    const endpointURL = new URL('/api/images', HOSTNAME);
+    endpointURL.searchParams.set('e', '/photos/' + id);
+
+    const response = await fetch(endpointURL, { headers });
+    const data = await response.json();
+
+    const image = extractImageObject(data);
+
+    return { image, alt: data.alt };
+  } catch (error) {
+    return { image: null, alt: '' };
   }
 };
