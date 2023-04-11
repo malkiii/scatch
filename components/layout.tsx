ee3d9a66add4b6a176b7b4ab217ac68d1b218a3c
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-11 18:17:17 +0100

    - Adding the footer.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
+import Footer from './Footer';
 import { FC, ReactNode } from 'react';
 import { useTheme } from 'next-themes';
 import Progressbar from './Progressbar';
@@ -20,6 +21,7 @@ const Layout: FC<layoutProps> = ({ children }) => {
       <Progressbar />
       <Navbar />
       {children}
+      <Footer />
     </>
   );
 };
