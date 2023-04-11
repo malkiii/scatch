d60090f92e98a9f2dde5b5a0addfcb74c157f293
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-11 18:17:52 +0100

    - Removing the footer from the component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Footer from '@components/Footer';
 import ScrollToTopButton from '@components/ScrollToTopButton';
 import {
   HeroSection,
@@ -10,16 +9,13 @@ import {
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <div className="px-8 overflow-x-hidden">
-        <HeroSection />
-        <ExploreSection />
-        <AlbumsSection />
-        <EditingSection />
-        <ScrollToTopButton />
-      </div>
-      <Footer />
-    </>
+    <div className="px-8 overflow-x-hidden">
+      <HeroSection />
+      <ExploreSection />
+      <AlbumsSection />
+      <EditingSection />
+      <ScrollToTopButton />
+    </div>
   );
 };
 export default HomePage;
