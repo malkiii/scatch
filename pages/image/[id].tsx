e9958ed19f1cf493769fc4a3be0cf360311ee641
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-13 14:45:17 +0100

    - Create the image page.

---
+++ pages/image/[id].tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { ResponseImage } from '@utils/types';
+import { fetchPhoto } from '@utils/fetchImages';
+import { GetServerSideProps, NextPage } from 'next';
+import ImagePageContent from '@components/Search/ImagePageContent';
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const id = query.id as string;
+  const { image, alt } = await fetchPhoto(id);
+  return {
+    props: { image, alt, key: id }
+  };
+};
+
+type PageProps = {
+  image: ResponseImage;
+  alt: string;
+};
+
+const imagePage: NextPage<PageProps> = props => {
+  const title = `Scatch | ${props.alt || props.image.photographer}`;
+  return (
+    <div className="main-container bg-cs-change p-5 my-6 rounded-xl shadow-2xl">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <ImagePageContent {...props} />
+    </div>
+  );
+};
+export default imagePage;
