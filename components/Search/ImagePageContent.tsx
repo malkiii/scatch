45b9b54926581506e1df02e292591d7e83e76207
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-13 14:44:18 +0100

    - Creating the /image/id page content.

---
+++ components/Search/ImagePageContent.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+import { FC, useRef } from 'react';
+import { ResponseImage } from '@utils/types';
+import { useBlurhashImage } from '@hooks/useBlurhashImage';
+import {
+  SaveButton,
+  DownloadButton,
+  PhotographerName
+} from '@components/Search/ImageLayer';
+
+const ImageNavbar: FC<{ image: ResponseImage }> = ({ image }) => {
+  return (
+    <div className="flex items-center justify-between pb-5">
+      <PhotographerName
+        name={image.photographer}
+        className="text-2xl font-bold text-dark dark:text-white"
+      />
+      <div className="flex items-center gap-3">
+        <SaveButton className="cs-change" />
+        <DownloadButton image={image} content="text" className="cs-change" />
+      </div>
+    </div>
+  );
+};
+
+type PageContentProps = {
+  image: ResponseImage;
+  alt: string;
+};
+
+const RenderedImage: FC<PageContentProps> = ({ image, alt }) => {
+  const { width, height, src } = image;
+  const imageContainerRef = useRef<HTMLDivElement>(null);
+
+  useBlurhashImage(imageContainerRef, src);
+
+  const containerClassName =
+    'mx-auto w-full ' + (height > width ? 'md:w-1/2' : '');
+
+  return (
+    <div ref={imageContainerRef} className={containerClassName}>
+      <Image
+        src={src}
+        width={width}
+        height={height}
+        className="bg-image"
+        alt={'scatch ' + alt}
+      />
+    </div>
+  );
+};
+
+const ImagePageContent: FC<PageContentProps> = props => {
+  return (
+    <>
+      <ImageNavbar image={props.image} />
+      <RenderedImage {...props} />
+    </>
+  );
+};
+
+export default ImagePageContent;
