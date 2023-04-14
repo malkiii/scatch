cf1ca4e661625606a90916ef22663d68990e13db
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-14 20:26:57 +0100

    - Import the container ref type.

--- hooks/useBlurhashImage.ts
+++ hooks/useBlurhashImage.ts
@@ -1,7 +1,6 @@
-import { RefObject, useEffect } from 'react';
+import { useEffect } from 'react';
 import getBlurhash from '@utils/blurhash';
-
-type ContainerRef = RefObject<HTMLDivElement>;
+import { ContainerRef } from '@utils/types';
 
 export const useBlurhashImage = (containerRef: ContainerRef, src: string) => {
   const preloadImageURL = `${src}?auto=compress&cs=tinysrgb&w=4`;
