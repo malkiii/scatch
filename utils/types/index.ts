7578256e3e78ba94066fd69545501c79d7c03bed
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-14 20:26:08 +0100

    - Defiend the image modal types.

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -1,8 +1,23 @@
+import { RefObject } from 'react';
+
 export type ResponseImage = {
   id: number;
   width: number;
   height: number;
   photographer: string;
   avgColor: string;
+  src: string;
+};
+
+export type ContainerRef = RefObject<HTMLDivElement>;
+
+export type ModalActions<T = () => void> = {
+  next?: T;
+  prev?: T;
+  close: T;
+};
+export type ModalImage = {
+  width: number;
+  height: number;
   src: string;
 };
