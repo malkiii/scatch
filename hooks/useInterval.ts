121fb46773ffa31f5523981b4100f2062e0d2df6
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-17 20:29:53 +0100

    - Return a loops counter.

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -1,21 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Dispatch, SetStateAction } from 'react';
 
-type IndexState = [number, Dispatch<SetStateAction<number>>];
+type IndexState = [number, Dispatch<SetStateAction<number>>, number];
 type IntervalHook = (itemsNumber: number, delay: number) => IndexState;
 
 export const useInterval: IntervalHook = (itemsNumber, delay) => {
   const [currentIndex, setCurrentIndex] = useState<number>(-1);
+  const [loopsCounter, setLoopsCounter] = useState<number>(-1);
 
   useEffect(() => {
     if (currentIndex == -1) return;
+    if (currentIndex == 0) setLoopsCounter(loopsCounter + 1);
     const interval = setInterval(() => {
       setCurrentIndex(currentIndex == itemsNumber - 1 ? 0 : currentIndex + 1);
     }, delay);
     return () => {
       clearInterval(interval);
     };
-  });
+  }, [currentIndex]);
 
-  return [currentIndex, setCurrentIndex];
+  return [currentIndex, setCurrentIndex, loopsCounter];
 };
