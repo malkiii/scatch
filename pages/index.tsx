c812c212b3c03468b346e6a2371ff2b99d0233d0
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-17 19:41:54 +0100

    - Set a background color for the main container

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const MeshGradientBackground = () => {
   return (
     <motion.div
       animate={{ opacity: 1, transition: { duration: 0.8, type: 'tween' } }}
-      className="fixed opacity-0 top-0 left-0 w-full min-h-screen -z-10 mesh-grandient"
+      className="fixed dark:bg-dark bg-white opacity-0 top-0 left-0 w-full min-h-screen -z-10 mesh-grandient"
     ></motion.div>
   );
 };
