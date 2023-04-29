636a8b823ab276bb77b2800146b85a3d62a50b7d
malkiAbdoo <abdo.malkiep@gmail.com>
2023-04-29 14:14:32 +0100

    - Remove the cs-icon CDN.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="preload"
-          href="https://raw.githubusercontent.com/malkiii/color-scheme-icon-cdn/master/font/theme-font.woff2"
-          as="font"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/malkiii/color-scheme-icon-cdn/cs-icon.min.css"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
