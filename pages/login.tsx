e449126c78df66a18e6b2ca4f965a4bc320f19d5
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-01 15:46:49 +0100

    - Create the login page.

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import LoginForm from '@/components/Forms/LoginForm';
+
+const LoginPage: NextPage = () => {
+  return (
+    <div className="font-rubik flex items-center justify-center bg-mesh-grandient px-4">
+      <div className="text-center text-dark p-4 w-[460px] bg-white shadow-2xl rounded-xl">
+        <h1 className="my-4 text-4xl capitalize">welcome to scatch</h1>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
