5deac9b0354b84c07bc27489e9a95830902f3eee
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-01 15:41:14 +0100

    - Inisialize the session provider of next-auth.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,25 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import Layout from '@/components/layout';
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  router,
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preload" as="image" href="/logotype.svg" />
         <title>Scatch</title>
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionProvider session={session}>
+        <Layout route={router.pathname}>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </ThemeProvider>
   );
 }
