97393ce5d9ebe0afe835b0f512c92bd72699ecca
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-03 19:13:12 +0100

    - Use the AuthProviders component.

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,53 +1,20 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import { signIn } from 'next-auth/react';
-import { FcGoogle as GoogleIcon } from 'react-icons/fc';
-import { BsFacebook as FacebookIcon } from 'react-icons/bs';
-
-const providers = [
-  {
-    name: 'Google',
-    icon: <GoogleIcon size={25} />,
-    style: { color: '#000', backgroundColor: '#fff', border: '1px solid gray' },
-    handleSignIn: () => signIn('google')
-  },
-  {
-    name: 'Facebook',
-    icon: <FacebookIcon size={25} />,
-    style: { backgroundColor: '#1877F2' },
-    handleSignIn: () => signIn('facebook')
-  }
-];
-
-const LoginProviders: FC = () => {
-  return (
-    <>
-      {providers.map(({ name, icon, style, handleSignIn }, id) => (
-        <button
-          key={id}
-          type="button"
-          style={style}
-          onClick={handleSignIn}
-          className="flex items-center justify-center gap-3 w-full text-white p-3 rounded-3xl font-semibold"
-        >
-          {icon} Continue with {name}
-        </button>
-      ))}
-    </>
-  );
-};
+import AuthProviders from './authProviders';
 
 const CredentialInputs: FC = () => {
   return (
     <>
       <input
         type="email"
+        name="user[email]"
         placeholder="Email"
         className="credential-input"
         required
       />
       <input
         type="password"
+        name="user[password]"
         placeholder="Password"
         className="credential-input"
         pattern=".{6,}"
@@ -60,8 +27,12 @@ const CredentialInputs: FC = () => {
 
 const LoginForm = () => {
   return (
-    <form onSubmit={undefined} className="flex flex-col gap-4">
-      <LoginProviders />
+    <form
+      onSubmit={undefined}
+      className="flex flex-col gap-4"
+      autoComplete="on"
+    >
+      <AuthProviders message="Continue with" />
       <div className="relative z-10">
         <span className="absolute inset-0 m-auto vertical-line-gradient h-px z-0"></span>
         <span className="relative bg-white px-4">OR</span>
