a64e47fa33b3ad9e0591901d289fe852fab35acc
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-03 21:58:27 +0100

    - Force the color scheme to be light.

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,10 @@ import LoginForm from '@/components/Forms/LoginForm';
 const LoginPage: NextPage = () => {
   return (
     <div className="font-rubik flex items-center justify-center bg-mesh-grandient px-4">
-      <div className="text-center text-dark p-4 w-[460px] bg-white shadow-2xl rounded-xl">
+      <div
+        className="text-center text-dark p-4 w-[460px] bg-white shadow-2xl rounded-xl"
+        style={{ colorScheme: 'light' }}
+      >
         <h1 className="my-4 text-4xl capitalize">welcome to scatch</h1>
         <LoginForm />
       </div>
