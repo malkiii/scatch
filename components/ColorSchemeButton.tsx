530c43a6400d553ec190c212ebb694034d45a9b2
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-04 17:56:51 +0100

    - Update the button style.

--- components/ColorSchemeButton.tsx
+++ components/ColorSchemeButton.tsx
@@ -23,7 +23,7 @@ const ColorSchemeButton: FC<ButtonProps> = props => {
       Dark
       <div
         className={
-          'relative rounded-2xl after:absolute after:top-1/2 after:translate-y-[-48%] after:h-[90%] after:rounded-circle after:aspect-square group-hover:border-theme group-hover:after:bg-theme transition-inherit after:transition-inherit after:duration-500 border-2 dark:border-white border-dark dark:after:bg-white after:bg-dark dark:after:left-px after:left-[57%] ' +
+          'relative rounded-2xl after:absolute after:top-1/2 after:translate-y-[-48%] after:h-[90%] after:rounded-circle after:aspect-square group-hover:border-theme group-hover:after:bg-theme transition-inherit after:transition-inherit after:duration-500 border-2 dark:border-white border-dark dark:after:bg-white after:bg-dark after:left-px dark:after:left-[57%] ' +
           props.buttonClassName
         }
       ></div>
