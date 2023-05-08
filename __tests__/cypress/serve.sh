567c9dabd24685644aa8c4eee56a38719fa6fb3c
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-08 16:00:39 +0100

    test: update the serve script

--- __tests__/cypress/serve.sh
+++ __tests__/cypress/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if curl -IsS http://localhost:3000 &> /dev/null ; then
+if nc -z localhost 3000 ; then
   yarn cypress
 else
   echo -e "\nRun \x1b[33m\"yarn dev\"\x1b[0m to start localhost:3000 server.\n"
