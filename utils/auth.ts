18cc3d981820dae367823dacc0c2fa127e2494c0
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-09 16:30:19 +0100

    chore: add the prisma adapter for next-auth

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,9 +1,14 @@
 import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const env = process.env as Record<string, string>;
+
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   pages: { signIn: '/login' },
   providers: [
     GoogleProvider({
