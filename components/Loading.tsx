fad0b84f20d8de5b480c118708e55e0fd5d5970a
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-12 11:56:04 +0100

    chore: create loading animation components

---
+++ components/Loading.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { CgSpinner as Spinner } from 'react-icons/cg';
+
+export const SpinnerAnimation: FC<{ size: number }> = ({ size }) => {
+  return <Spinner size={size} className="animate-spin" />;
+};
+
+export const PulseAnimation: FC = () => {
+  return (
+    <div className="flex gap-x-3 items-center justify-center">
+      <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.00s_infinite]"></span>
+      <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.25s_infinite]"></span>
+      <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.50s_infinite]"></span>
+      <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.75s_infinite]"></span>
+    </div>
+  );
+};
