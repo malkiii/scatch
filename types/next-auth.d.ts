eb20511fe3692093ba817348ad289b9a7510ed82
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-12 12:05:28 +0100

    fix(types): add next-auth declaration

---
+++ types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import { User } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+  }
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: User;
+  }
+}
