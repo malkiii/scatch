278e57700f7a2d65184230d87dd1ba88eaf9a59f
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-13 18:02:24 +0100

    - Update the cs button style

--- components/ColorSchemeButton.tsx
+++ components/ColorSchemeButton.tsx
@@ -9,9 +9,7 @@ type ButtonProps = {
 
 const ColorSchemeButton: FC<ButtonProps> = props => {
   const { setTheme } = useTheme();
-  const buttonStyle =
-    'relative group hover:text-theme transition-colors duration-200 ' +
-    props.containerClassName;
+  const buttonStyle = 'relative group ' + props.containerClassName;
 
   function toggleTheme() {
     const preferDarkTheme = document.documentElement.classList.contains('dark');
@@ -20,13 +18,8 @@ const ColorSchemeButton: FC<ButtonProps> = props => {
 
   return (
     <button data-test="cs-button" className={buttonStyle} onClick={toggleTheme}>
-      Dark
-      <div
-        className={
-          'relative rounded-2xl after:absolute after:top-1/2 after:translate-y-[-48%] after:h-[90%] after:rounded-circle after:aspect-square group-hover:border-theme group-hover:after:bg-theme transition-inherit after:transition-inherit after:duration-500 border-2 dark:border-white border-dark dark:after:bg-white after:bg-dark after:left-px dark:after:left-[57%] ' +
-          props.buttonClassName
-        }
-      ></div>
+      {props.children}
+      <div className={'cs-button ' + props.buttonClassName}></div>
     </button>
   );
 };
