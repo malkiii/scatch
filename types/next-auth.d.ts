6aed2c86c1018b37a66c01171648e11699a5b679
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-13 21:20:37 +0100

    - Update the JWT type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,9 @@
 import { User } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
+import { JWT, DefaultJWT } from 'next-auth/jwt';
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    id: string;
+    sub: string;
   }
 }
 
