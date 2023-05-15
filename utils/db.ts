47061950d6733b25b5d425a6446f2444bc577530
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-15 17:34:25 +0100

    - Return db

--- utils/db.ts
+++ utils/db.ts
@@ -4,10 +4,10 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query']
-  });
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['query'] });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export const db = prisma;
