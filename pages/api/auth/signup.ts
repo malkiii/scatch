0fea6e4aff2837e31c10f4888cc35ec746b9a6d3
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-16 17:00:38 +0100

    - Create signup request handler

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,34 @@
+import { db } from '@/utils/db';
+import { hash } from 'bcryptjs';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function signUpRequest(req: NextApiRequest, res: NextApiResponse) {
+  const errorResposne = (type: string, message: string, status: number) => {
+    res.status(status).json({ error: type, message });
+  };
+
+  if (req.method !== 'POST') return errorResposne('method', 'Method Not Allowed', 405);
+
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await db.user.findUnique({
+      where: { email }
+    });
+
+    if (existingUser) return errorResposne('email', 'User already exists', 409);
+
+    // Hash the password
+    const hashedPassword = await hash(password, 12);
+
+    // Create the new user
+    const newUser = await db.user.create({
+      data: { name, email, password: hashedPassword }
+    });
+
+    res.status(201).json({ message: 'User created successfully', user: newUser });
+  } catch (error) {
+    return errorResposne('server', 'Internal Server Error', 500);
+  }
+}
