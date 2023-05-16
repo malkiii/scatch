9907e908e170b666fc2b049357ddd5bf68b3b031
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-16 19:49:24 +0100

    chore(config): remove server components external packages

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
   images: {
     domains: ['images.pexels.com']
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client']
-  },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     API_TOKEN: process.env.API_KEY
