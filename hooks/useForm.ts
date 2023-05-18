cfecc20a3b9b1ff6cd6e3a3a780ccc7eed83c145
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-18 09:24:48 +0100

    - Add setError function

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -1,18 +1,24 @@
+import { WithFormError } from '@/types';
 import { Dispatch, SetStateAction, useState } from 'react';
 
 type Data<T> = {
   data: T;
   setData: Dispatch<SetStateAction<T>>;
+  setError: (error: WithFormError['error']) => void;
   handleInput: (e: any) => void;
 };
 
-export const useForm = <T>(formData: T): Data<T> => {
-  const [data, setData] = useState<T>(formData);
+export const useForm = <T>(formData: T): Data<T & WithFormError> => {
+  const [data, setData] = useState<T & WithFormError>({ ...formData, error: undefined });
 
-  function handleInput(e: any) {
+  const handleInput: Data<null>['handleInput'] = e => {
     const { id, value } = e.target;
-    setData(prevFormData => ({ ...prevFormData, [id]: value }));
-  }
+    setData(prevData => ({ ...prevData, [id]: value }));
+  };
 
-  return { data, setData, handleInput };
+  const setError: Data<null>['setError'] = error => {
+    setData(prevData => ({ ...prevData, error }));
+  };
+
+  return { data, setData, setError, handleInput };
 };
