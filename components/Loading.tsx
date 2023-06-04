7fdfcd6cfc24f87e8b70f4f719b6c90f9e92c50f
malkiAbdoo <abdo.malkiep@gmail.com>
2023-06-04 11:23:40 +0100

    - Update the types

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -7,7 +7,7 @@ export const SpinnerAnimation: FC<{ size: number }> = ({ size }) => {
 
 export const PulseAnimation: FC = () => {
   return (
-    <div className="flex items-center justify-center gap-x-3">
+    <div className="mt-5 flex items-center justify-center gap-x-3">
       <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.00s_infinite]"></span>
       <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.25s_infinite]"></span>
       <span className="loading-circle animate-[pulse_1s_cubic-bezier(0.4,0,0.6,1)_.50s_infinite]"></span>
--- utils/index.ts
+++ utils/index.ts
@@ -1,30 +1,26 @@
-import { z } from 'zod';
 import { SignUpFormData } from '@/types';
-import { ImageAPIRequestQuerySchema } from '@/types/zod';
+import { ImageAPIRequestQuery } from '@/types';
 
-export function getImageFetchURL(requestQuery: z.infer<typeof ImageAPIRequestQuerySchema>): URL {
+const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL as string;
+
+export function getImageFetchURL(requestQuery: ImageAPIRequestQuery): URL {
   const { API_ENDPOINT } = process.env;
-  const endpointName = '/v1/' + requestQuery.endpoint;
+  const { endpoint, query, orientation, page, per_page } = requestQuery;
 
+  const endpointName = '/v1' + endpoint;
   const endpointURL = new URL(endpointName, API_ENDPOINT);
   if (endpointName.includes('/photos')) return endpointURL;
 
-  endpointURL.searchParams.set('page', (requestQuery.page || 1).toString());
-  endpointURL.searchParams.set('per_page', (requestQuery.per_page || 24).toString());
+  endpointURL.searchParams.set('page', (page || 1).toString());
+  endpointURL.searchParams.set('per_page', (per_page || 24).toString());
 
-  const orientation = requestQuery.orientation || 'all';
-
-  if (endpointName.includes('/search')) endpointURL.searchParams.set('query', endpointName);
-
-  if (orientation != 'all') endpointURL.searchParams.set('orientation', orientation);
+  if (query && endpointName.includes('/search')) endpointURL.searchParams.set('query', query);
+  if (orientation && orientation != 'all') endpointURL.searchParams.set('orientation', orientation);
 
   return endpointURL;
 }
 
 type CachedData = { user?: any; message: string; error?: string };
-
-const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL as string;
-
 export async function signUp(data: SignUpFormData): Promise<CachedData> {
   const endpointURL = new URL('/api/auth/signup', NEXT_PUBLIC_APP_URL);
 
@@ -38,20 +34,6 @@ export async function signUp(data: SignUpFormData): Promise<CachedData> {
   });
 
   return await response.json();
-}
-
-export async function translateToEnglish(text: string): Promise<string> {
-  const API_TOKEN = process.env.API_TOKEN as string;
-
-  const endpointURL = new URL('/api/translate', NEXT_PUBLIC_APP_URL);
-  endpointURL.searchParams.set('text', text);
-
-  const headers = { token: API_TOKEN };
-
-  const response = await fetch(endpointURL, { headers });
-  const { translation } = await response.json();
-
-  return translation;
 }
 
 export function createUsernameParam(username: string): string {
