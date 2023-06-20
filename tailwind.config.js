7054b613a3a05b09ac72b19d5df4d4b291784a96
malkiAbdoo <abdo.malkiep@gmail.com>
2023-06-20 13:09:30 +0100

    - Add the album modal grid layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,8 @@ module.exports = {
         progressbar: '0 0 10px, 0 0 5px'
       },
       gridTemplateColumns: {
-        images: 'repeat(auto-fill, minmax(320px, 1fr))'
+        images: 'repeat(auto-fill, minmax(320px, 1fr))',
+        'album-modal': 'repeat(auto-fill, minmax(200px, 1fr))'
       }
     }
   },
