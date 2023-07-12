1e83199f1dd94dfda76432f800723e62e3ecbded
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-12 13:29:05 +0100

    perf: remove the preload links in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-// prettier-ignore
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" crossOrigin="Anonymous" />
-        <link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap" crossOrigin="Anonymous" />
         <link rel="preload" as="image" href="/logotype.svg" />
         <link rel="preload" as="image" href="/mark.svg" />
       </Head>
