0dceaffe77aa97509660fb812a8c025f259d5dc4
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-24 21:05:10 +0100

    chore(pages): using the new form components

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,22 +1,22 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { ScatchMark } from '@/components/Forms/FormItems';
+import { ScatchMark } from '@/components/Forms/FormComponents';
 import SignUpForm from '@/components/Forms/SignUpForm';
 
 const RegisterPage: NextPage = () => {
   return (
-    <div className="flex h-screen w-full items-center overflow-hidden p-7">
+    <div className="flex h-screen w-full items-center overflow-hidden from-base-300 to-transparent p-7 dark:bg-gradient-to-b">
       <Head>
         <title>Join to Scatch</title>
       </Head>
       <div className="mx-auto w-[540px] text-center">
         <ScatchMark className="mx-auto" />
-        <div className="my-9">
-          <h2 className="auth-title">join to scatch</h2>
-          <div className="text-neutral-500">
+        <div className="my-7">
+          <h2 className="my-4 text-3xl sm:text-4xl">join to scatch</h2>
+          <div className="text-base-content/50">
             Already have an account?{' '}
-            <Link href="/login" className="text-dark underline dark:text-white">
+            <Link href="/login" className="link text-base-content">
               Login
             </Link>
           </div>
