0bffcfd9d6592593f20f3dd0e71c6edfc44348f1
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-25 22:26:59 +0100

    chore(component): update the styles

--- components/Search/SearchImageModal.tsx
+++ components/Search/SearchImageModal.tsx
@@ -3,7 +3,7 @@ import { useSession } from 'next-auth/react';
 import { ModalActions, ResponseImage } from '@/types';
 import { useAlbumModal } from '@/hooks/useAlbumModal';
 import AlbumModal from '@/components/Dashboard/AlbumModal';
-import { DownloadButton, ImageTitle, SaveButton } from '../ImageLayer';
+import { DownloadButton, ImageTitle, SaveButton } from '../ImageLayers';
 import ImageModal from '../ImageModal';
 
 type ImageNavProps = {
@@ -13,18 +13,16 @@ type ImageNavProps = {
 const ImageNavbar: FC<ImageNavProps> = ({ image, onClick }) => {
   const { data: session } = useSession();
   const userId = session?.user.id;
+
   const { showAlbumModal, albumModalProps, toggleAlbumModal } = useAlbumModal(image, userId!);
+
   return (
     <AlbumModal show={showAlbumModal} toggle={toggleAlbumModal} {...albumModalProps}>
       <div className="flex items-center justify-between pb-5" onClick={onClick}>
         <ImageTitle title={image.photographer} className="text-white" />
         <div className="flex items-center gap-3">
-          <SaveButton
-            {...albumModalProps}
-            toggleAlbumModal={toggleAlbumModal}
-            className="cs-fixed"
-          />
-          <DownloadButton {...albumModalProps} content="text" className="cs-fixed" />
+          <SaveButton {...albumModalProps} toggleAlbumModal={toggleAlbumModal} />
+          <DownloadButton content="text" {...albumModalProps} />
         </div>
       </div>
     </AlbumModal>
@@ -43,4 +41,5 @@ const SearchImageModal: FC<ModalProps> = ({ index, image, modalActions }) => {
     </ImageModal>
   );
 };
+
 export default SearchImageModal;
--- components/Search/SearchInput.tsx
+++ components/Search/SearchInput.tsx
@@ -11,6 +11,7 @@ type SuggestionsMenuProps = {
 
 const SuggestionsMenu: FC<SuggestionsMenuProps> = ({ value, onClick }) => {
   const searchHistory = useSearchHistory(value);
+
   const [focusIndex, setFocusIndex] = useState<number>(-1);
 
   function handleKeyDown({ key }: any) {
@@ -37,22 +38,28 @@ const SuggestionsMenu: FC<SuggestionsMenuProps> = ({ value, onClick }) => {
     };
   });
 
+  if (!searchHistory.length) return null;
   return (
     <div className="absolute left-0 top-full z-[101] hidden w-full translate-y-3 group-focus-within:block">
-      <ul className="w-full rounded-xl shadow-3xl">
+      <ul className="menu w-full rounded-xl bg-neutral shadow-2xl">
         {searchHistory.map((searchValue, index) => (
           <li
             key={index}
-            className={cn('menu-suggestion', { 'focus': focusIndex == index })}
             onMouseEnter={() => setFocusIndex(index)}
             onMouseDown={() => onClick(searchValue)}
             onMouseLeave={() => setFocusIndex(-1)}
             onKeyDown={handleKeyDown}
           >
-            <SearchIcon size={21} />
-            <span className="inline-block overflow-hidden text-ellipsis whitespace-nowrap">
-              {searchValue}
-            </span>
+            <div
+              className={cn('gap-x-3 px-3 text-lg transition-none', {
+                'bg-base-content/10': focusIndex == index
+              })}
+            >
+              <SearchIcon size={21} />
+              <span className="inline-block overflow-hidden text-ellipsis whitespace-nowrap">
+                {searchValue}
+              </span>
+            </div>
           </li>
         ))}
       </ul>
@@ -60,10 +67,8 @@ const SuggestionsMenu: FC<SuggestionsMenuProps> = ({ value, onClick }) => {
   );
 };
 
-type InputProps = {
-  value?: string;
-};
-const SearchInput: FC<InputProps> = ({ value }) => {
+type InputProps = { value?: string };
+const SearchInputSection: FC<InputProps> = ({ value }) => {
   const [inputValue, setInputValue] = useState<string>(value || '');
   const { inputRef, triggerTheSearch, handleEnter } = useSearchTrigger();
 
@@ -74,30 +79,34 @@ const SearchInput: FC<InputProps> = ({ value }) => {
 
   return (
     <div className="mx-auto px-4 pt-20 text-center sm:pt-28">
-      <h2>
-        Search for <span className="theme-gradient bg-clip-text text-transparent">images</span>
+      <h2 className="text-4xl md:text-5xl">
+        Search for{' '}
+        <span className="bg-gradient-to-br from-primary to-primary-focus bg-clip-text pr-1 text-transparent">
+          images
+        </span>
       </h2>
-      <div className="bg-cs-change group relative mx-auto my-7 flex max-w-[730px] items-center rounded-3xl text-lg shadow-lg transition-colors">
+      <div className="group relative mx-auto my-7 flex max-w-[730px] items-center gap-x-2">
         <input
           type="search"
           ref={inputRef}
           value={inputValue}
           placeholder="Search.."
-          className="h-full flex-grow bg-transparent px-4 py-2 outline-none"
+          className="theme-input h-full px-4 py-2 shadow-xl"
           onInput={e => setInputValue(e.currentTarget.value)}
           onKeyDown={handleEnter}
           autoComplete="off"
           required
         />
         <button
-          className="theme-btn rounded-inherit px-5 py-2 sm:px-7"
+          className="theme-btn animate-none px-4 py-2 shadow-xl"
           onClick={async () => await triggerTheSearch()}
         >
-          <SearchIcon size={30} className="inline text-center" />
+          <SearchIcon size={30} />
         </button>
         <SuggestionsMenu value={inputValue} onClick={handleMouseClick} />
       </div>
     </div>
   );
 };
-export default SearchInput;
+
+export default SearchInputSection;
