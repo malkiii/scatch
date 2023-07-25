b1367c757ee8ef61a48fdfd59e288f597b585436
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-25 22:17:57 +0100

    fix(layers): update the styles

--- components/ImageLayers.tsx
+++ components/ImageLayers.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Image as UserImage } from '@prisma/client';
 import { useSession } from 'next-auth/react';
-import { CgMathPlus, CgSoftwareDownload } from 'react-icons/cg';
+import { CgSoftwareDownload as DownloadIcon, CgMathPlus as PlusIcon } from 'react-icons/cg';
 import { FaRegHeart as FavoriteOutlineIcon, FaHeart as FavoriteSolidIcon } from 'react-icons/fa';
 import { ResponseImage } from '@/types';
 import { cn } from '@/utils';
@@ -36,7 +36,6 @@ type WithClassName = {
 type ImageTitleProps = {
   title: string;
 } & WithClassName;
-
 export const ImageTitle: FC<ImageTitleProps> = ({ title, className }) => {
   return <strong className={cn('font-normal', className)}>{title}</strong>;
 };
@@ -57,10 +56,10 @@ export const SaveButton: FC<SaveButtonPros> = ({ userId, toggleAlbumModal, class
   return (
     <button
       title="Save this image in your album"
-      className={cn('image-layer-btn', className)}
+      className={cn('p-2', className || 'theme-btn')}
       onClick={handleClick}
     >
-      <CgMathPlus size={logoSize} />
+      <PlusIcon size={logoSize} />
     </button>
   );
 };
@@ -83,7 +82,7 @@ export const FavoriteButton: FC<FavoriteButtonProps> = ({ image, userId, classNa
   return (
     <button
       title={favorite ? 'Remove from my favorite images' : 'Add to my favorite images'}
-      className={cn('image-layer-btn', className)}
+      className={cn('p-2', className || 'theme-btn')}
       onClick={setAsFavoriteImage}
     >
       {favorite && <FavoriteSolidIcon size={logoSize} />}
@@ -96,7 +95,6 @@ type DownloadButtonPops = {
   content: 'text' | 'icon';
 } & WithImageAndUserId &
   WithClassName;
-
 export const DownloadButton: FC<DownloadButtonPops> = props => {
   const { image, userId, content, className } = props;
   const downloadURL = `${image.src}?cs=srgb&dl=scatch-${image.id}.jpg&fm=jpg`;
@@ -112,57 +110,57 @@ export const DownloadButton: FC<DownloadButtonPops> = props => {
     <a
       href={downloadURL}
       title="Download the image"
-      className={cn('image-layer-btn', className)}
+      className={cn('p-2', className || 'theme-btn')}
       onClick={handleClick}
     >
-      {content == 'icon' ? <CgSoftwareDownload size={logoSize} /> : 'Download'}
+      {content == 'icon' ? <DownloadIcon size={logoSize} /> : 'Download'}
     </a>
   );
 };
 
-type SubLayerProps = WithImageAndUserId &
-  WithChildren & {
-    linkProps?: LayerProps['linkProps'];
-    toggleAlbumModal?: Function;
-  };
-const InnerImageLayer: FC<SubLayerProps> = props => {
+type NestedLayerProps = {
+  linkProps?: LayerProps['linkProps'];
+  toggleAlbumModal?: Function;
+} & WithImageAndUserId &
+  WithChildren;
+
+const InnerImageLayer: FC<NestedLayerProps> = props => {
   const { image, userId, toggleAlbumModal, linkProps, children } = props;
-  const buttonProps = { userId, image, toggleAlbumModal: toggleAlbumModal! };
-  const buttonClassName = 'cs-fixed absolute right-5 top-5';
+
+  const btnProps = { userId, image, toggleAlbumModal: toggleAlbumModal! };
+  const btnClassName =
+    'btn btn-outline text-white border-white hover:border-white hover:text-black hover:bg-white h-fit min-h-0 border-2';
   const title = isUserImage(image) ? image.albumName : image.photographer;
+
   return (
     <Link {...linkProps} data-test="modal-link" className="relative">
       {children}
-      <div className="image-layout-cover">
+      <div className="before:focus-gradient absolute inset-0 opacity-0 transition-opacity before:pointer-events-none before:absolute before:inset-0 hover:opacity-100">
         {isUserImage(image) ? (
-          <FavoriteButton {...buttonProps} className={buttonClassName} />
+          <FavoriteButton {...btnProps} className={cn(btnClassName, 'absolute right-5 top-5')} />
         ) : (
-          <SaveButton {...buttonProps} className={buttonClassName} />
+          <SaveButton {...btnProps} className={cn(btnClassName, 'absolute right-5 top-5')} />
         )}
         <div className="absolute bottom-0 flex w-full items-center justify-between p-5">
           <ImageTitle title={title} className="text-white" />
-          <DownloadButton {...buttonProps} content="icon" className="cs-fixed" />
+          <DownloadButton content="icon" {...btnProps} className={btnClassName} />
         </div>
       </div>
     </Link>
   );
 };
 
-const OuterImageLayer: FC<SubLayerProps> = ({ image, userId, toggleAlbumModal, children }) => {
+const OuterImageLayer: FC<NestedLayerProps> = ({ image, userId, toggleAlbumModal, children }) => {
   const buttonProps = { userId, image, toggleAlbumModal: toggleAlbumModal! };
   const title = isUserImage(image) ? image.albumName : image.photographer;
-  const buttonClassName = 'cs-change';
+
   return (
     <div key={image.id}>
       <ImageTitle title={title} className="block py-3 pl-1 text-white" />
       {children}
       <div className="flex w-full items-center justify-between px-2 pt-3">
-        {isUserImage(image) ? (
-          <FavoriteButton {...buttonProps} className={buttonClassName} />
-        ) : (
-          <SaveButton {...buttonProps} className={buttonClassName} />
-        )}
-        <DownloadButton {...buttonProps} content="text" className="cs-change" />
+        {isUserImage(image) ? <FavoriteButton {...buttonProps} /> : <SaveButton {...buttonProps} />}
+        <DownloadButton content="text" {...buttonProps} />
       </div>
     </div>
   );
@@ -173,10 +171,12 @@ type LayerProps = {
   hasMobileSize: boolean;
 } & WithImageAndUserId &
   WithChildren;
+
 export const ImageLayer: FC<LayerProps> = props => {
+  const { image, linkProps, children } = props;
+
   const { data: session } = useSession();
   const userId = session?.user.id;
-  const { image, linkProps, children } = props;
 
   const layerProps = { userId, image, linkProps, children };
   const imageLayer = (layerProps: any) =>
