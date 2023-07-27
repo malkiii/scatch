33e23310b341d7481f74492ec5227b5c2a73c4b0
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-27 22:30:11 +0100

    chore: update the search image modal component

--- components/Search/SearchImageModal.tsx
+++ components/Search/SearchImageModal.tsx
@@ -6,38 +6,28 @@ import AlbumModal from '@/components/Dashboard/AlbumModal';
 import { DownloadButton, ImageTitle, SaveButton } from '../ImageLayers';
 import ImageModal from '../ImageModal';
 
-type ImageNavProps = {
-  image: ResponseImage;
-  onClick?: () => void;
-};
-const ImageNavbar: FC<ImageNavProps> = ({ image, onClick }) => {
-  const { data: session } = useSession();
-  const userId = session?.user.id;
-
-  const { showAlbumModal, albumModalProps, toggleAlbumModal } = useAlbumModal(image, userId!);
-
-  return (
-    <AlbumModal show={showAlbumModal} toggle={toggleAlbumModal} {...albumModalProps}>
-      <div className="flex items-center justify-between pb-5" onClick={onClick}>
-        <ImageTitle title={image.photographer} className="text-white" />
-        <div className="flex items-center gap-3">
-          <SaveButton {...albumModalProps} toggleAlbumModal={toggleAlbumModal} />
-          <DownloadButton content="text" {...albumModalProps} />
-        </div>
-      </div>
-    </AlbumModal>
-  );
-};
-
 type ModalProps = {
   index: number;
   image: ResponseImage;
   modalActions: ModalActions;
 };
 const SearchImageModal: FC<ModalProps> = ({ index, image, modalActions }) => {
+  const { data: session } = useSession();
+  const userId = session?.user.id;
+
+  const { showAlbumModal, albumModalProps, toggleAlbumModal } = useAlbumModal(image, userId!);
+
   return (
     <ImageModal key={index} image={image} actions={modalActions}>
-      <ImageNavbar image={image} onClick={modalActions.close} />
+      <AlbumModal show={showAlbumModal} toggle={toggleAlbumModal} {...albumModalProps}>
+        <div className="flex items-center justify-between pb-5" onClick={modalActions.close}>
+          <ImageTitle title={image.photographer} className="text-white" />
+          <div className="flex items-center gap-3">
+            <SaveButton {...albumModalProps} toggleAlbumModal={toggleAlbumModal} />
+            <DownloadButton content="text" {...albumModalProps} />
+          </div>
+        </div>
+      </AlbumModal>
     </ImageModal>
   );
 };
