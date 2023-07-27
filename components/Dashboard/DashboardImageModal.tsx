5967e83c18f731626bc9c18c634d1f44a2a9234c
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-27 22:58:39 +0100

    chore(dashboard): update the image modal component

--- components/Dashboard/DashboardImageModal.tsx
+++ components/Dashboard/DashboardImageModal.tsx
@@ -5,35 +5,28 @@ import { ModalActions } from '@/types';
 import { DownloadButton, FavoriteButton, ImageTitle } from '../ImageLayers';
 import ImageModal from '../ImageModal';
 
-type ImageNavProps = {
-  image: UserImage;
-  onClick?: () => void;
-};
-const ImageNavbar: FC<ImageNavProps> = ({ image, onClick }) => {
-  const { data: session } = useSession();
-  const userId = session?.user.id;
-  const albumModalProps = { userId, image };
-  return (
-    <div className="flex items-center justify-between pb-5" onClick={onClick}>
-      <ImageTitle title={image.albumName} className="text-white" />
-      <div className="flex items-center gap-3">
-        <FavoriteButton {...albumModalProps} className="cs-fixed" />
-        <DownloadButton {...albumModalProps} content="text" className="cs-fixed" />
-      </div>
-    </div>
-  );
-};
-
 type ModalProps = {
   index: number;
   image: UserImage;
   modalActions: ModalActions;
 };
 const DashboardImageModal: FC<ModalProps> = ({ index, image, modalActions }) => {
+  const { data: session } = useSession();
+  const userId = session?.user.id;
+
+  const albumModalProps = { userId, image };
+
   return (
     <ImageModal key={index} image={image} actions={modalActions}>
-      <ImageNavbar image={image} onClick={modalActions.close} />
+      <div className="flex items-center justify-between pb-5" onClick={modalActions.close}>
+        <ImageTitle title={image.albumName} className="text-white" />
+        <div className="flex items-center gap-3">
+          <FavoriteButton {...albumModalProps} />
+          <DownloadButton content="text" {...albumModalProps} />
+        </div>
+      </div>
     </ImageModal>
   );
 };
+
 export default DashboardImageModal;
