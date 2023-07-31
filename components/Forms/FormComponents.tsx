8e5ba6b4fb1daab418f3b3db778aae7cd59adfc5
malkiAbdoo <abdo.malkiep@gmail.com>
2023-07-31 21:05:34 +0100

    feat(forms): start using  package

--- components/Forms/FormComponents.tsx
+++ components/Forms/FormComponents.tsx
@@ -1,37 +1,23 @@
-import { FC, ReactNode, useState } from 'react';
-import Image from 'next/image';
+import { FC, InputHTMLAttributes, PropsWithChildren, useState } from 'react';
 import { signIn } from 'next-auth/react';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import { BiHide as HideIcon, BiShow as ShowIcon } from 'react-icons/bi';
 import { BsFacebook as FacebookIcon } from 'react-icons/bs';
 import { FcGoogle as GoogleIcon } from 'react-icons/fc';
 import { IoIosWarning as WarningIcon } from 'react-icons/io';
 import { cn } from '@/utils';
 
-type MarkProps = {
-  className?: string;
-};
-export const ScatchMark: FC<MarkProps> = ({ className }) => {
-  return (
-    <Image
-      src="/mark.svg"
-      alt="scatch mark"
-      width={65}
-      height={65}
-      className={cn('logo', className)}
-    />
-  );
-};
-
-const iconSize = 25;
+const providerIconSize = 25;
 const providers = [
   {
     name: 'Google',
-    icon: <GoogleIcon size={iconSize} />,
+    icon: <GoogleIcon size={providerIconSize} />,
     className: 'text-dark bg-white border border-neutral-400 dark:border-none hover:bg-neutral-50',
     handleSignIn: () => signIn('google', { callbackUrl: '/' })
   },
   {
     name: 'Facebook',
-    icon: <FacebookIcon size={iconSize} />,
+    icon: <FacebookIcon size={providerIconSize} />,
     className: 'text-white bg-[#1877F2] hover:bg-[#166ee1]',
     handleSignIn: () => signIn('facebook', { callbackUrl: '/' })
   }
@@ -68,27 +54,53 @@ export const AuthProviders: FC<AuthProvidersProps> = ({ text }) => {
   );
 };
 
-type SubmitButtonProps = {
-  text: string;
-  isSubmitting: boolean;
-  onClick?: (e?: MouseEvent) => void;
-};
-export const SubmitButton: FC<SubmitButtonProps> = ({ text, isSubmitting, onClick }) => {
-  function handleClick(e: any) {
-    if (isSubmitting) e.preventDefault();
-    else onClick ? onClick(e) : null;
-  }
+type SubmitButtonProps = PropsWithChildren<{
+  loading: boolean;
+}>;
+export const SubmitButton: FC<SubmitButtonProps> = ({ loading, children }) => {
   return (
-    <button type="submit" onClick={handleClick} className="theme-btn">
-      {isSubmitting ? <div className="loading loading-spinner w-[25px]"></div> : text}
+    <button type="submit" onClick={e => loading && e.preventDefault()} className="theme-btn">
+      {loading ? <div className="loading loading-spinner w-[24px]"></div> : children}
     </button>
   );
 };
 
-export const ErrorMessage: FC<{ children: ReactNode }> = ({ children }) => {
+export const ErrorMessage: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="mt-2 flex w-full items-center justify-center gap-x-2 text-sm text-error md:text-base">
-      <WarningIcon /> {children}
+    <p role="alert" className="mt-2 w-full text-center text-sm text-error md:text-base">
+      <WarningIcon className="mb-[3px] mr-1 inline-flex" /> {children}
+    </p>
+  );
+};
+
+interface InputProps extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
+  register: () => UseFormRegisterReturn<string>;
+  error?: string;
+}
+export const Input: FC<InputProps> = ({ children, className, register, error, ...props }) => {
+  return (
+    <div className="relative w-full">
+      {children}
+      <input {...register()} {...props} className={cn('theme-input', className, { error })} />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
     </div>
+  );
+};
+
+type PasswordInputProps = Omit<InputProps, 'type' | 'name'>;
+export const PasswordInput: FC<PasswordInputProps> = ({ className, ...props }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const iconSize = 22;
+
+  return (
+    <Input {...props} type={showPassword ? 'text' : 'password'} className={cn('pr-12', className)}>
+      <button
+        type="button"
+        className="absolute right-4 top-3 flex items-center"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? <ShowIcon size={iconSize} /> : <HideIcon size={iconSize} />}
+      </button>
+    </Input>
   );
 };
--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -1,77 +1,49 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { signIn } from 'next-auth/react';
-import { LoginFormData, WithFormError } from '@/types';
-import { cn } from '@/utils';
-import { useForm } from '@/hooks/useForm';
-import { AuthProviders, ErrorMessage, SubmitButton } from './FormComponents';
+import { useForm } from 'react-hook-form';
+import { RegisteredInputs } from '@/types';
+import { AuthProviders, Input, PasswordInput, SubmitButton } from './FormComponents';
 
-type CredentialInputsProps = {
-  data: LoginFormData & WithFormError;
-  handleInput: (e: any) => void;
-};
-const CredentialInputs: FC<CredentialInputsProps> = ({ data, handleInput }) => {
-  const { email, password, error } = data;
-  return (
-    <>
-      <div>
-        <input
-          id="email"
-          type="email"
-          name="user[email]"
-          placeholder="Email"
-          className={cn('theme-input', { 'error': error == 'Email' })}
-          value={email}
-          onInput={handleInput}
-          required
-        />
-        {error == 'Email' && <ErrorMessage>This user does not exists!</ErrorMessage>}
-      </div>
-      <div>
-        <input
-          id="password"
-          type="password"
-          name="user[password]"
-          placeholder="Password"
-          className={cn('theme-input', { 'error': error == 'Password' })}
-          pattern=".{6,}"
-          title="Password must be at least 6 characters"
-          value={password}
-          onInput={handleInput}
-          required
-        />
-        {error == 'Password' && <ErrorMessage>Invalid password!</ErrorMessage>}
-      </div>
-    </>
-  );
+type LoginFormData = {
+  email: string;
+  password: string;
 };
 
 const LoginForm: FC = () => {
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-  const form = useForm<LoginFormData>({ email: '', password: '' });
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting }
+  } = useForm<LoginFormData>();
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-    form.setError(undefined);
-
-    // Login with the new account
-    const { email, password } = form.data;
+  const submitHandler = handleSubmit(async ({ email, password }) => {
     const response = await signIn('credentials', { email, password, redirect: false });
     if (response) {
-      const { error } = response as WithFormError;
-      if (error) {
-        form.setError(error);
-        setIsSubmitting(false);
-      } else location.pathname = '/';
+      const { error: errorMessage } = response;
+      if (!errorMessage) return (location.pathname = '/');
+      setError('email', { message: errorMessage });
+    }
+  });
+
+  const inputs: RegisteredInputs<LoginFormData> = {
+    email: {
+      register: () => register('email', { required: true }),
+      error: errors.email?.message
+    },
+    password: {
+      register: () => register('password', { required: true }),
+      error: errors.password?.message
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+    <form onSubmit={submitHandler} className="flex flex-col gap-4">
       <AuthProviders text="Continue with" />
       <div className="divider my-2">OR</div>
-      <CredentialInputs {...form} />
-      <SubmitButton {...{ text: 'Login', isSubmitting }} />
+      <Input type="email" {...inputs.email} placeholder="Email" />
+      <PasswordInput {...inputs.password} placeholder="Password" />
+      <SubmitButton loading={isSubmitting}>Login</SubmitButton>
     </form>
   );
 };
