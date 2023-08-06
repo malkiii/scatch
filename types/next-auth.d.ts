054380df3d91369ab7806c69dccc922718533265
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-06 18:46:18 +0100

    chore(tRPC): update the routers inputs

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,22 @@
-import { User } from 'next-auth';
-import { DefaultJWT, JWT } from 'next-auth/jwt';
+import type { Session, User } from 'next-auth';
+import type { DefaultJWT, JWT } from 'next-auth/jwt';
 
 declare module 'next-auth/jwt' {
   interface JWT {
+    name: string;
+    email: string;
+    picture?: string;
     sub: string;
   }
 }
 
 declare module 'next-auth' {
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    image?: string | null;
+  }
   interface Session {
     user: User;
   }
