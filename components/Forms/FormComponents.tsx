226059e817c9acf3324c2aaef5b8b3fc446922df
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-08 15:43:49 +0100

    fix: return the form files to fix the conflix problem

---
+++ components/Forms/FormComponents.tsx
@@ -0,0 +1,192 @@
+import {
+  ButtonHTMLAttributes,
+  ChangeEvent,
+  FC,
+  InputHTMLAttributes,
+  PropsWithChildren,
+  useRef,
+  useState
+} from 'react';
+import type { User } from 'next-auth';
+import { signIn } from 'next-auth/react';
+import Cropper, { ReactCropperElement } from 'react-cropper';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import { BiHide as HideIcon, BiShow as ShowIcon } from 'react-icons/bi';
+import { BsFacebook as FacebookIcon } from 'react-icons/bs';
+import { FcGoogle as GoogleIcon } from 'react-icons/fc';
+import { IoMdCamera as CameraIcon, IoIosWarning as WarningIcon } from 'react-icons/io';
+import { cn, getUserAvatar, resizeAndCropImage } from '@/utils';
+import { trpc } from '@/utils/trpc';
+import { Modal } from '../Modal';
+
+const providerIconSize = 25;
+const providers = [
+  {
+    name: 'Google',
+    icon: <GoogleIcon size={providerIconSize} />,
+    className: 'text-dark bg-white border border-neutral-400 dark:border-none hover:bg-neutral-50',
+    handleSignIn: () => signIn('google', { callbackUrl: '/' })
+  },
+  {
+    name: 'Facebook',
+    icon: <FacebookIcon size={providerIconSize} />,
+    className: 'text-white bg-[#1877F2] hover:bg-[#166ee1]',
+    handleSignIn: () => signIn('facebook', { callbackUrl: '/' })
+  }
+];
+
+type AuthProvidersProps = {
+  text: string;
+};
+export const AuthProviders: FC<AuthProvidersProps> = ({ text }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  function handleClick(target: HTMLButtonElement, handler: Function) {
+    if (isLoading) return;
+    setIsLoading(true);
+    target.innerHTML = '<div class="loading loading-spinner w-[25px]"></div>';
+    handler();
+  }
+
+  return (
+    <>
+      {providers.map(({ name, icon, className, handleSignIn }, id) => (
+        <button
+          key={id}
+          type="button"
+          onClick={e => handleClick(e.currentTarget, handleSignIn)}
+          className={cn(
+            'flex w-full items-center justify-center gap-3 rounded-lg p-3 text-center font-semibold transition-colors',
+            className
+          )}
+        >
+          {icon} {text} {name}
+        </button>
+      ))}
+    </>
+  );
+};
+
+interface SubmitButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading: boolean;
+}
+export const SubmitButton: FC<SubmitButtonProps> = props => {
+  const { loading, className, onClick, children, ...buttonProps } = props;
+  return (
+    <button
+      type="submit"
+      onClick={e => (loading ? e.preventDefault() : onClick && onClick(e))}
+      className={cn('theme-btn', className)}
+      {...buttonProps}
+    >
+      {loading ? <div className="loading loading-spinner w-[24px]"></div> : children}
+    </button>
+  );
+};
+
+export const ErrorMessage: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <p role="alert" className="mt-2 w-full text-center text-sm text-error md:text-base">
+      <WarningIcon className="mb-[3px] mr-1 inline-flex" /> {children}
+    </p>
+  );
+};
+
+interface InputProps extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
+  register: () => UseFormRegisterReturn<string>;
+  error?: string;
+}
+export const Input: FC<InputProps> = ({ children, className, register, error, ...props }) => {
+  return (
+    <div className="relative w-full">
+      {children}
+      <input {...register()} {...props} className={cn('theme-input', className, { error })} />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </div>
+  );
+};
+
+type PasswordInputProps = Omit<InputProps, 'type' | 'name'>;
+export const PasswordInput: FC<PasswordInputProps> = ({ children, className, ...props }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const iconSize = 22;
+
+  return (
+    <Input {...props} type={showPassword ? 'text' : 'password'} className={cn('pr-12', className)}>
+      {children}
+      <button
+        type="button"
+        className="absolute bottom-3 right-4 flex items-center"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? <ShowIcon size={iconSize} /> : <HideIcon size={iconSize} />}
+      </button>
+    </Input>
+  );
+};
+
+type AvatarPickerProps = {
+  user: User;
+  className?: string;
+};
+export const AvatarPicker: FC<AvatarPickerProps> = ({ user, className }) => {
+  const [image, setImage] = useState<string | null>(null);
+  const [savedImage, setSavedImage] = useState<typeof image>(null);
+  const cropperRef = useRef<ReactCropperElement>(null);
+
+  const { mutateAsync: changeUserImage, isLoading } = trpc.changeUserImage.useMutation();
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    files && files.length && setImage(await resizeAndCropImage(files[0], 420, 420));
+  };
+
+  const saveImage = async () => {
+    const cropper = cropperRef.current?.cropper;
+    if (!cropper) return;
+
+    const imageURL = cropper.getCroppedCanvas().toDataURL();
+    await changeUserImage({ userId: user.id, image: imageURL });
+
+    setSavedImage(imageURL);
+    setImage(null);
+  };
+
+  return (
+    <div className={className}>
+      <div className="group relative overflow-hidden rounded-circle">
+        <img
+          src={savedImage || getUserAvatar(user.image, true)}
+          width={300}
+          height={300}
+          className="select-none"
+          alt={`${user.name} avatar`}
+          referrerPolicy="no-referrer"
+        />
+        <label className="absolute inset-0 flex cursor-pointer items-center justify-center bg-primary/40 text-white opacity-0 transition-opacity group-hover:opacity-100">
+          <CameraIcon size={40} />
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={handleFileChange}
+            className="hidden"
+            // you have to add the value property to use the file input multiple times
+            value=""
+          />
+        </label>
+      </div>
+      {image && (
+        <Modal className="relative p-7" close={() => setImage(null)}>
+          <Cropper ref={cropperRef} src={image} aspectRatio={1} className="mx-auto max-w-[420px]" />
+          <SubmitButton
+            type="button"
+            loading={isLoading}
+            onClick={saveImage}
+            className="mt-4 w-full"
+          >
+            Save
+          </SubmitButton>
+        </Modal>
+      )}
+    </div>
+  );
+};
---
+++ components/Forms/SignUpForm.tsx
@@ -0,0 +1,124 @@
+import { FC } from 'react';
+import { User } from '@prisma/client';
+import { signIn } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { RegisteredInputs } from '@/types';
+import { authValidationPatterns } from '@/data/constants';
+import { AuthProviders, Input, PasswordInput, SubmitButton } from './FormComponents';
+
+type SignUpFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+type SignUpResponse = { user?: User; error?: string };
+export async function signUp(data: SignUpFormData): Promise<SignUpResponse> {
+  const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL as string;
+  const endpointURL = new URL('/api/auth/signup', NEXT_PUBLIC_APP_URL);
+
+  const { firstName, lastName, email, password } = data;
+  const name = (firstName.trim() + ' ' + lastName.trim()).trim();
+
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, password })
+  });
+
+  return await response.json();
+}
+
+const SignUpForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting }
+  } = useForm<SignUpFormData>();
+
+  const submitHandler = handleSubmit(async data => {
+    // trying to sign up
+    const { user, error: errorMessage } = await signUp(data);
+    if (!user) return setError('email', { message: errorMessage });
+
+    // Login with the new account
+    const { email, password } = data;
+    signIn('credentials', { email, password, callbackUrl: '/' });
+  });
+
+  const inputs: RegisteredInputs<SignUpFormData> = {
+    firstName: {
+      register: () =>
+        register('firstName', {
+          required: 'Specify the first name',
+          pattern: {
+            value: authValidationPatterns.name,
+            message: 'No spaces or special characters!'
+          },
+          minLength: {
+            value: 3,
+            message: 'First name is too short!'
+          },
+          maxLength: {
+            value: 15,
+            message: 'First name is too long!'
+          }
+        }),
+      error: errors.firstName?.message
+    },
+    lastName: {
+      register: () =>
+        register('lastName', {
+          pattern: {
+            value: authValidationPatterns.name,
+            message: 'No spaces and special characters!'
+          },
+          maxLength: {
+            value: 15,
+            message: 'Last name is too long!'
+          }
+        }),
+      error: errors.lastName?.message
+    },
+    email: {
+      register: () =>
+        register('email', {
+          required: 'Specify your email address',
+          pattern: {
+            value: authValidationPatterns.email,
+            message: 'Invalid Email!'
+          }
+        }),
+      error: errors.email?.message
+    },
+    password: {
+      register: () =>
+        register('password', {
+          required: 'Add a password',
+          pattern: {
+            value: authValidationPatterns.password,
+            message: 'Password must be at least 6 characters'
+          }
+        }),
+      error: errors.password?.message
+    }
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="flex flex-col gap-y-4" autoComplete="on">
+      <AuthProviders text="Join using" />
+      <div className="divider my-2">OR</div>
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <Input {...inputs.firstName} placeholder="First name" />
+        <Input {...inputs.lastName} placeholder="Last name" />
+      </div>
+      <Input type="email" {...inputs.email} placeholder="Email" />
+      <PasswordInput {...inputs.password} placeholder="Password (min. 6 characters)" />
+      <SubmitButton loading={isSubmitting}>Sign Up</SubmitButton>
+    </form>
+  );
+};
+
+export default SignUpForm;
