9d9783460cde571339dafb34c142367da587bc0b
malkiAbdoo <abdo.malkiep@gmail.com>
2023-08-10 18:58:56 +0100

    test: update the cypress specs

--- __tests__/cypress/e2e/components/ColorSchemeButton.cy.ts
+++ __tests__/cypress/e2e/components/ColorSchemeButton.cy.ts
@@ -1,9 +1,11 @@
 describe('Color scheme button test', () => {
   beforeEach('Visit the home page', () => cy.visit('/'));
   it('Toggle the color scheme', () => {
-    const htmlElementClasses = document.documentElement.classList;
-    const currentTheme = htmlElementClasses.contains('dark') ? 'light' : 'dark';
-    cy.get('[data-test="cs-button"]').click();
-    cy.get('html').should('have.css', 'color-scheme', currentTheme == 'dark' ? 'light' : 'dark');
+    cy.get('html').then(element => {
+      const currentTheme = element.attr('data-theme');
+
+      cy.get('[data-test="cs-button"]').first().click();
+      cy.get('html').should('have.attr', 'data-theme', currentTheme === 'dark' ? 'light' : 'dark');
+    });
   });
 });
